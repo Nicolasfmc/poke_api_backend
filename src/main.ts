e3f4f24d0dcf06ca86013a7f0c56db4ce310d3e4
nicolasfmc <nicolasfmc1@gmail.com>
2024-07-22 09:51:31 -0300

    fix 2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import cors from 'cors'
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors)
-
+  app.enableCors({ origin: ['*'] })
   await app.listen(5432);
 }
 main();
