3ca97a722862a18845500993d164f3ba25dc4914
nicolasfmc <nicolasfmc1@gmail.com>
2024-07-22 09:37:29 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import cors from 'cors'
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors())
+
   await app.listen(5432);
 }
 main();
