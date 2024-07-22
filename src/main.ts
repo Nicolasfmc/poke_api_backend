93944ab795bc478d7a9a97391b3efb12a3f85499
nicolasfmc <nicolasfmc1@gmail.com>
2024-07-22 11:08:08 -0300

    fix: origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', '*'] })
+  app.enableCors({ origin: ['http://localhost:3000', 'https://poke-consulting.vercel.app/', '*'] })
   await app.listen(5432);
 }
 main();
